1fab65aa473fc54277a4c667b48043c1975d8cdd
htmlguy <htmlguy999@gmail.com>
2015-05-24 10:25:08 -0400

    MultiScreen Test

--- test-game/objects.js
+++ test-game/objects.js
@@ -1,6 +1,14 @@
 objects.titlescreen = function () {
     this.update = function () {
-        canvas.draw_image("mainbg", gamedata.ticks % 2, gamedata.ticks % 2)
-        if (keypresses.enter) console.log("X")
+        canvas.draw_image("mainbg", 0, 0)
+        canvas.draw_image("version", 0, 430)
+        if (keypresses.enter) select_screen (1)
+    }
+}
+
+objects.randomscreen = function () {
+    this.update = function () {
+        canvas.draw_image("keytest", 0, 0)
+        if (keypresses.enter && keys.alt) alert("A")
     }
 }
