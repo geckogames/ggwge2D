af4ea19178bb57850e610c2c3363575c1a14d58c
htmlguy <htmlguy999@gmail.com>
2015-05-24 14:49:27 -0400

    Notice on objects

--- test-game/objects.js
+++ test-game/objects.js
@@ -1,3 +1,22 @@
+/*
+    ggwge2D test game objects code - the objects for testgame
+    Copyright 2015 GeckoGames.
+
+    This file is part of ggwge2D test game.
+
+    ggwge2D test game is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ggwge2D test game is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ggwge2D test game.  If not, see <http://www.gnu.org/licenses/>.
+*/
 objects.titlescreen = function () {
     this.update = function () {
         canvas.draw_image("mainbg", 0, 0)
